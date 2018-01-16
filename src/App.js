e5121b9ec8fdba5a9671cf43475b7be52fc3ed72
Aaron Silverstein <adsilvercomp@Aarons-MacBook-Pro.local>
2018-01-16 14:41:06 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Search from "./sections/Search";
+// import Articles from "./sections/Articles";
+// import Saved from "./sections/Saved";
+
+
+
+
+const App = () =>
+
+  <div>
+      <Search/>  
+      {/* <Articles/>
+      <Saved/>      */}
+  </div>
+
+
+export default App;
