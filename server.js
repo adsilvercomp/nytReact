92a32372512f0d3c346bf7cb963f891b30c9d420
Aaron Silverstein <adsilvercomp@Aarons-MacBook-Pro.local>
2018-01-27 00:35:43 -0500

    update

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const articleController = require("./controllers/articleController")
+const articleController = require("./controllers/articleController");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Configure body parser for AJAX requests
+// Configure body parser for axios requests
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // Serve up static assets
